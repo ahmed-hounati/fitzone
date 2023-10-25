67b033c2c88cb000fe841eb60518279dbb0f16c9
ahmed-hounati <hounatiahmed@gmail.com>
2023-10-25 12:16:11 +0100

    last-version

--- js/script.js
+++ js/script.js
@@ -4,12 +4,22 @@ const navBar = document.querySelector(".nav-bar");
 menuicon.addEventListener("click", () => {
   menuicon.classList.toggle("active");
   navBar.classList.toggle("active");
-});
+})
 
-document.querySelectorAll(".nav-bar li").forEach((item) => {
-  item.addEventListener("click", () => {
-    menuicon.classList.remove("active");
-    navBar.classList.remove("active");
-  });
-});
+document.querySelectorAll(".nav-bar li").forEach(n => n.addEventListener("click", () => {
+  menuicon.classList.remove("active");
+  navBar.classList.remove("active");
+}))
 
+const faq = document.querySelectorAll("h4");
+
+faq.forEach((faq) => {
+  faq.addEventListener('click', () => {
+    const answer = faq.nextElementSibling;
+    if (answer.style.display === 'block') {
+      answer.style.display = 'none';
+    } else {
+      answer.style.display = 'block';
+    }
+  })
+})
